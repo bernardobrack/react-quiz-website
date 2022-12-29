c901b4d634ec7ac970068ba49716b3baef614f8f
Bernardo Brack <be.gbrack@gmail.com>
2022-12-29 01:32:17 -0300

    adds not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import {Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
 import QuizPage from './components/QuizPage';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/quiz" element={<QuizPage />}/>
+        <Route path="*" element={<NotFound />}/>
       </Routes>
     </main>
   </>
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,5 @@
+import "./NotFound.css";
+
+export default function NotFound() {
+    return <p id="error-message">Couldn't find the page</p>
+}
